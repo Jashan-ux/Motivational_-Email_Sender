e7b7cbdcadb789992ba865e1fdbf988864960943
Jashan Aneja <jashaninsan777@gmail.com>
2024-10-28 22:42:40 +0530

    Add files via upload

---
+++ quotes.py
@@ -0,0 +1,32 @@
+import smtplib
+import random as rm
+import datetime as dt
+
+MY_EMAIL = "jashananeja777@gmail.com"
+PASSWORD = "nvjkitfmqrvgafkw"
+
+
+from datetime import datetime as dt
+
+today = dt.today()
+
+weekday_number = today.weekday()
+
+
+weekdays = ["Monday", "Tuesday","Wednesday", "Thursday"  , "Friday","Saturday", "Sunday"]
+
+weekday_name = weekdays[weekday_number]
+
+
+with open("quotes.txt") as quote_file:
+    all_quotes = quote_file.readlines()
+    quote = rm.choice(all_quotes)
+
+with smtplib.SMTP("smtp.gmail.com" ,587) as connection:
+    connection.starttls()
+    connection.login(user = MY_EMAIL ,password = PASSWORD)
+    connection.sendmail(
+        from_addr= MY_EMAIL ,
+        to_addrs = "mehakaneja075@gmail.com" ,
+        msg= f"Subject:{weekday_name} Motivation\n\n{quote}")
+    
